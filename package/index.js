7a211abd4a5dbef891f3bc72468ffc5455eaa1df
sachinjacob123 <sachin.jacob@joinditto.in>
2023-07-20 15:35:30 +0530

    added tingle js

---
+++ package/index.js
@@ -0,0 +1,31 @@
+// Function to open the modal
+function openModal() {
+  var modal = new tingle.modal({
+    footer: false,
+    stickyFooter: false,
+    closeMethods: ["button"], //"overlay", "button", "escape"
+    closeLabel: "Close",
+    onOpen: function () {
+      console.log("modal open");
+    },
+    onClose: function () {
+      console.log("modal closed");
+    },
+    beforeClose: function () {
+      // here's goes some logic
+      // e.g. save content before closing the modal
+      return true; // close the modal
+    },
+  });
+
+  // set content
+  modal.setContent(
+    `<iframe style="width: 100%;height:100%;" src="https://test-timely.joinditto.in/event/test/book?hotline=True"></iframe>`
+  );
+
+  modal.open();
+}
+
+// Add event listener to the button
+const openModalButton = document.getElementById("openModalButton");
+openModalButton.addEventListener("click", openModal);
